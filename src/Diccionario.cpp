a35e5df02e353c67dfe8077a4a72ee4ed8a23ac9
Andrés <ajgm94@gmail.com>
2015-01-19 19:15:39 +0100

    Añadido el Esqueleto del T.D.A Diccionario

---
+++ src/Diccionario.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespaces std;
+
+
+Diccionario::Diccionario(){/*No hace nada*/}
+
+int Diccionario::size() const 
+{
+	return datos.size();//Esto sólo devuelve las letras 
+}
